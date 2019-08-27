da8447e3c7100b6ad1b572a42f2c1944436294ca
Raul <raul@ideas24h.com>
2019-08-27 22:03:28 +0200

    csss

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -15,19 +15,13 @@
         </div>
         <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
           <div class="text-sm lg:flex-grow">
-            <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-              <g-link class="mx-2 hover:text-gray-600" to="/">Home</g-link>
-            </a>
-            <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-              <g-link class="mx-2 hover:text-gray-600" to="/about">Acerca de</g-link>
-            </a>
-            <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
-              <g-link class="mx-2 hover:text-gray-600" to="/afiliados">Afiliados</g-link>
-            </a>
+              <g-link class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4" to="/">Home</g-link>
+              <g-link class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4" to="/about">Acerca de</g-link>
+              <g-link class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4" to="/afiliados">Afiliados</g-link>
           </div>
-    <!-- <div>
-      <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Download</a>
-    </div> -->
+          <!-- <div>
+            <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Download</a>
+          </div> -->
         </div>
       </nav>  
     </header>
